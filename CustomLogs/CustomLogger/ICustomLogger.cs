1167732936648ac7148172c5f5168b7541ff655f
daniel-kleszczynski <danielkleszczynski@gmail.com>
2020-11-30 15:15:07 +0100

    Initial ISink and ICustomLogger interfaces

---
+++ CustomLogs/CustomLogger/ICustomLogger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLogs
+{
+    public interface ICustomLogger
+    {
+        void Start();
+        void Log(string message, [CallerFilePath]string callerPath = "", [CallerMemberName]string callerName = "", [CallerLineNumber]int callerLine = -1);
+    }
+}
---
+++ CustomLogs/CustomLogger/Sinks/ISink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomLogs.Sinks
+{
+    public interface ISink : IDisposable
+    {
+        ConcurrentQueue<string[]> LogQueue { get; }
+        void Enable(string programName, string userName, int delayedSeconds);
+        void Flush();
+    }
+}
